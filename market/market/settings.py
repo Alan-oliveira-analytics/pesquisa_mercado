0659ae97072560a1a3fa8c208587c3868cc2639b
Alan Oliveira <alanoliveira@MacBook-Air-de-Alan.local>
2026-04-05 10:46:41 -0300

    webscrapping pages

--- market/market/settings.py
+++ market/market/settings.py
@@ -19,7 +19,7 @@ ADDONS = {}
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Safari/605.1.15"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Concurrency and throttling settings
 #CONCURRENT_REQUESTS = 16
--- market/market/spiders/mercadolivre.py
+++ market/market/spiders/mercadolivre.py
@@ -5,6 +5,8 @@ class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     allowed_domains = ["lista.mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/monitor#D[A:monitor]"]
+    page_count = 1
+    max_page = 10
 
     def parse(self, response):
 
@@ -12,14 +14,26 @@ class MercadolivreSpider(scrapy.Spider):
 
         for product in products:
 
+            prices = product.css('span.andes-money-amount__fraction::text').getall()
+
             yield {
                 'title': product.css('a.poly-component__title::text').get(),
                 'review': product.css('span.poly-phrase-label::text').get(),
-                'price': product.css('span.andes-money-amount__fraction::text').getall(),
+                'new_price': prices[1] if len(prices) > 1 else None,
+                'old_price': prices[0] if len(prices) > 0 else None,
                 'percent_off': product.css('span.poly-price__disc_label::text').get(),
                 'installment_count' : product.css('span.poly-price__installments::text').get(),
-                'installment_amount': product.css('span.andes-money-amount__fraction::text').getall()
+                'installment_amount': prices[2] if len(prices) > 2 else None
             }
+
+            if self.page_count <= self.max_page:
+
+                next_page = response.css('li.andes-pagination__button andes-pagination__button--next a::attr(href)').get()
+
+                if next_page:
+                    self.page_count += 1
+
+                    yield scrapy.Request(url=next_page, callback=self.parse)
 
         pass
 
